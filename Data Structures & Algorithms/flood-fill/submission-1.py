0bd17d1a29a0e6ea8dfe3fefa9e043224c0731b2
Aleksandr Babarika <143314666+AlexAlexBabarika@users.noreply.github.com>
2026-09-14 14:19:17 +0200

    Add: flood-fill - submission-1

---
+++ Data Structures & Algorithms/flood-fill/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        h = len(image)
+        w = len(image[0])
+        locked = [[0] * w for i in range(h)]
+        def paintPixel(image: List[List[int]], locked, sr: int, sc: int, val: int, color: int):
+            if not ((0 <= sr < h) and (0 <= sc < w)): return
+            if locked[sr][sc]: return 
+            if image[sr][sc] != val: return
+
+            locked[sr][sc] = 1
+            image[sr][sc] = color 
+
+            paintPixel(image, locked, sr - 1, sc, val, color)
+            paintPixel(image, locked, sr, sc - 1, val, color)
+            paintPixel(image, locked, sr, sc + 1, val, color)
+            paintPixel(image, locked, sr + 1, sc, val, color)
+        
+        
+        paintPixel(image, locked, sr, sc, image[sr][sc], color)
+        return image
